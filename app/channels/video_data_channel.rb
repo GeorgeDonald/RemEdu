5bd9127a87446d744ddabeb332a7cdde73d945c0
George Donald <dongsy326@gmail.com>
2017-12-17 23:22:04 -0500

    deleted pry-related lines

--- app/channels/video_data_channel.rb
+++ app/channels/video_data_channel.rb
@@ -1,10 +1,8 @@
-require 'pry'
 class VideoDataChannel < ApplicationCable::Channel
   def subscribed
     stream_from "VideoDataChannel_#{params[:room]}"
   end
   def receive(data)
-    binding.pry
     VideoBuffer.create(video_data: data['video_data'])
     #ActionCable.server.broadcast("VideoDataChannel_#{params[:room]}", data)
   end
